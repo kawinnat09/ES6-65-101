675e4f6cfce09f8bd639891fb1cdd1d6dde05d7d
kawinnat09 <Kwinnathmayura@gmail.com>
2022-05-23 17:33:41 +0700

    update

---
+++ app1/app101.js
@@ -0,0 +1,14 @@
+//Declartive Variable
+let banana = null;
+let apple = null;
+let fruit = apple / banana;
+
+const mango = 10;
+{
+    let mango = 5;
+    console.log(mango);
+}
+
+//Recall Variable
+console.log(mango);
+//console.log(fruit);
---
+++ app1/app105.js
@@ -0,0 +1,3 @@
+//const multiline = "line1\n\line2\n\line3"
+const multiline = "line1\n" + "\line2\n" + "\line3"
+console.log(multiline)
